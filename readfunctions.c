ba07ee457868a769ec1adff2aa239d608b90b33f
Bruno <osorio.bruno@hotmail.com>
2016-11-05 04:26:24 -0200

    Added function to convert a CP entry to float/double
    This is (supposedly) more portable than manipulating bits directly or
    using union to convert bytes to float/double. That's because those other
    method rely on the endianness of the machine.

--- readfunctions.c
+++ readfunctions.c
@@ -1,5 +1,6 @@
 #include "readfunctions.h"
 #include "utf8.h"
+#include <math.h>
 
 uint8_t readu4(JavaClassFile* jcf, uint32_t* out)
 {
@@ -93,4 +94,54 @@ int32_t readFieldDescriptor(uint8_t* utf8_bytes, int32_t utf8_len)
     }
 
     return totalBytesRead;
+}
+
+float readConstantPoolFloat(cp_info* entry)
+{
+    if (entry->Float.bytes == 0x7F800000UL)
+        return INFINITY;
+
+    if (entry->Float.bytes == 0xFF800000UL)
+        return -INFINITY;
+
+    if ((entry->Float.bytes >= 0x7F800000UL && entry->Float.bytes <= 0x7FFFFFFFUL) ||
+        (entry->Float.bytes >= 0xFF800000UL && entry->Float.bytes <= 0xFFFFFFFFUL))
+    {
+        return NAN;
+    }
+
+    int32_t s = (entry->Float.bytes >> 31) == 0 ? 1 : -1;
+    int32_t e = (entry->Float.bytes >> 23) & 0xFF;
+    int32_t m = (e == 0) ?
+        (entry->Float.bytes & 0x7FFFFFUL) << 1 :
+        (entry->Float.bytes & 0x7FFFFFUL) | 0x800000;
+
+    return s * (float)ldexp((double)m, e - 150);
+}
+
+double readConstantPoolDouble(cp_info* entry)
+{
+    uint64_t bytes = entry->Double.high;
+    bytes <<= 32;
+    bytes |= entry->Double.low;
+
+    if (bytes == 0x7FF0000000000000ULL)
+        return INFINITY;
+
+    if (bytes == 0xFFF0000000000000ULL)
+        return -INFINITY;
+
+    if ((bytes >= 0x7FF0000000000001ULL && bytes <= 0x7FFFFFFFFFFFFFFFULL) ||
+        (bytes >= 0xFFF0000000000001ULL && bytes <= 0xFFFFFFFFFFFFFFFFULL))
+    {
+        return NAN;
+    }
+
+    int32_t s = (bytes >> 63) == 0 ? 1 : -1;
+    int32_t e = (bytes >> 52) & 0x7FF;
+    int64_t m = (e == 0) ?
+        (bytes & 0xFFFFFFFFFFFFFULL) << 1 :
+        (bytes & 0xFFFFFFFFFFFFFULL) | 0x10000000000000ULL;
+
+    return s * ldexp((double)m, e - 1075);
 }
--- readfunctions.h
+++ readfunctions.h
@@ -4,9 +4,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "javaclassfile.h"
+#include "constantpool.h"
 
 uint8_t readu4(struct JavaClassFile* jcf, uint32_t* out);
 uint8_t readu2(struct JavaClassFile* jcf, uint16_t* out);
 int32_t readFieldDescriptor(uint8_t* utf8_bytes, int32_t utf8_len);
+float readConstantPoolFloat(cp_info* entry);
+double readConstantPoolDouble(cp_info* entry);
 
 #endif // FILEPARSER_H
