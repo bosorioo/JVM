1e65a415a2f126c775280e12c3335535838a7cbd
Bruno <osorio.bruno@hotmail.com>
2016-11-06 11:48:27 -0200

    Updated sample file and example output

--- Sketch.java
+++ Sketch.java
@@ -5,31 +5,41 @@ import java.util.*;
 public class Sketch implements EventListener, RandomAccess {
 
 	private static final int[] values = {1, 2, 3, 4, 5};
-	
-	public long long_value = 65536 * 65536 * 655356 * 21;
-	public static long long_value_static = 218372183L * 1238L;
-	
-	public double[][] matrix = {
+
+	public float float_member;
+	public long long_member = 5910974510923776L;
+	public static long long_static_value = 98765432123456789L;
+
+	private String str;
+
+	private int [][][] matrix3d = {
+		{{1, 2},
+		{3, 4}},
+		{{5, 6},
+		{7, 8}}
+	};
+
+	public double[][] matrix2d = {
 		{123.456, -654.321, 987654321.123456789},
 		{Double.MAX_VALUE, Double.MIN_VALUE, Double.MIN_NORMAL},
 		{Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY}
 	};
-	
+
 	interface Test {
 		public void method1(int[] a);
 	}
 
-    public void print(int i) {
-        System.out.println(i);
-    }
-	
+	public void print(int i) {
+		System.out.println(i);
+	}
+
 	public long add(int a, float b) {
 		long result = (long)a + (long)b;
 		return result;
 	}
-    
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
 }
