3f78b71d9e31b06af3cc192929e3b031233792ce
Bruno <osorio.bruno@hotmail.com>
2016-11-11 16:19:17 -0200

    Added comments about "utf8bom" parameter

--- main.c
+++ main.c
@@ -12,6 +12,12 @@ int main(int argc, char* args[])
         return 0;
     }
 
+    // Enable user to give an extra parameter "utf8bom" to output the three
+    // byte sequence that tells that the data is encoded as UTF-8.
+    // This is required by some programs (like notepad) to correctly
+    // render UTF-8 characters. This is useful when the output of this
+    // program is redirected to a file. With this parameter given, the file
+    // will also contain the UTF-8 BOM.
     if (argc >= 3 && strcmp(args[2], "utf8bom"))
         printf("%c%c%c", 0xEF, 0xBB, 0xBF);
 
