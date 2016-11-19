3ca1a42dfe5d21624e1ce7e8ba39e58c0112158a
Bruno <osorio.bruno@hotmail.com>
2016-11-19 19:22:45 -0200

    Minor change

--- jvm.h
+++ jvm.h
@@ -6,10 +6,12 @@ typedef struct JavaVirtualMachine JavaVirtualMachine;
 #include <stdint.h>
 #include "methodarea.h"
 #include "javaclass.h"
+#include "framestack.h"
 
 struct JavaVirtualMachine
 {
     method_area ma;
+    FrameStack* frames;
 };
 
 void initJVM(JavaVirtualMachine* jvm, JavaClass* mainClass);
