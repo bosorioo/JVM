9835ce2953bc881d820b16c2643f00d9590adc59
Bruno <osorio.bruno@hotmail.com>
2016-11-19 17:12:39 -0200

    Added more command line options

--- main.c
+++ main.c
@@ -2,36 +2,68 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include "javaclassfile.h"
+#include "javaclass.h"
+#include "methodarea.h"
 
 int main(int argc, char* args[])
 {
     if (argc <= 1)
     {
         printf("First parameter should be the path to a .class file.\n");
+        printf("Following parameters could be:\n");
+        printf(" -c \t Shows the content of the .class file\n");
+        printf(" -r \t Execute the method 'main' from the class\n");
+        printf(" -b \t Adds UTF-8 BOM to the output\n");
         return 0;
     }
 
-    // Enable user to give an extra parameter "utf8bom" to output the three
-    // byte sequence that tells that the data is encoded as UTF-8.
+    uint8_t printClassContent = 0;
+    uint8_t executeClassMain = 0;
+    uint8_t includeBOM = 0;
+
+    int argIndex;
+
+    for (argIndex = 2; argIndex < argc; argIndex++)
+    {
+        if (strcmp(args[argIndex], "-c"))
+            printClassContent = 1;
+        else if (strcmp(args[argIndex], "-r"))
+            executeClassMain = 1;
+        else if (strcmp(args[argIndex], "-b"))
+            includeBOM = 1;
+        else
+            printf("Unknown argument #%d ('%s')\n", argIndex, args[argIndex]);
+    }
+
+    // If requested, outputs the three byte sequence that tells that
+    // the data is encoded as UTF-8.
     // This is required by some programs (like notepad) to correctly
     // render UTF-8 characters. This is useful when the output of this
     // program is redirected to a file. With this parameter given, the file
     // will also contain the UTF-8 BOM.
-    if (argc >= 3 && strcmp(args[2], "utf8bom"))
+    if (includeBOM)
         printf("%c%c%c", 0xEF, 0xBB, 0xBF);
 
     // Open .class file given as parameter
-    JavaClassFile jcf;
-    openClassFile(&jcf, args[1]);
+    JavaClass jc;
+    openClassFile(&jc, args[1]);
 
-    // If everything went fine, print its content
-    if (jcf.status == STATUS_OK)
-        printClassFileInfo(&jcf);
-    // Otherwise, show what went wrong
-    else
-        printClassFileDebugInfo(&jcf);
+    // If something went wrong, show debug information
+    if (jc.status != STATUS_OK)
+        printClassFileDebugInfo(&jc);
+    // Otherwise, is requested by the user, print the class content
+    else if (printClassContent)
+        printClassFileInfo(&jc);
 
-    closeClassFile(&jcf);
+    if (executeClassMain)
+    {
+        method_area ma;
+        initMethodArea(&ma);
+        addClassToMethodArea(&ma, &jc);
+    }
+
+
+
+    closeClassFile(&jc);
     return 0;
 }
